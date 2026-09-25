ff9a69b7a0c4646f0e074896ef32c5a88ffad670
shashank <116015929+Shashank280804@users.noreply.github.com>
2026-09-25 15:37:32 +0530

    Learned print statements

---
+++ src/JavaBasics/PrintStatements.java
@@ -0,0 +1,10 @@
+package JavaBasics;
+
+public class PrintStatements {
+    public static void main(String[] args) {
+        System.out.println("Hello I am shashank");
+        System.out.println("Age:" + 23);
+        System.out.println(10 + 20);
+    }
+
+}
